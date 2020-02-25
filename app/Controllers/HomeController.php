2baa9aa405e5221d544d5a44795c262236157316
Mohamed jinas <j@live.mv>
2020-02-26 04:56:49 +0500

    fixed router

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -7,20 +7,8 @@ class HomeController extends Controller
 {
     public function index()
     {
-        //$request = Container::get('request');
+    
 
-        //$request->get('name');
-
-        $db = Container::get('database');
-        $names = $db->from('users')->get();
-        $shihaam = $db->from('users')->where('id', 2)->one();
-
-        $data = [
-            'users' => $names,
-            'user' => $shihaam,
-            'memory_used' => mem_usage() . "KB"
-        ];
-
-        jsonify($data);
+        jsonify("hello");
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -10,16 +10,3 @@
 */
 
 $router->get('', '\App\Controllers\HomeController@index');
-
-
-$router->get('posts','\App\Controllers\PostsController@getposts');
-$router->post('posts/create', '\App\Controllers\PostsController@addPost');
-
-$router->get('postsfactory', '\App\Controllers\PostsController@postFactory');
-
-$router->get('stubtest', '\App\Controllers\PostsController@test');
-
-$router->get('/test', function(){
-
-    echo 'hello';
-});
